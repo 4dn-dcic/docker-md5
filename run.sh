2d90bf8e7f77476d871c311b16222d249f785e49
SooLee <duplexa@gmail.com>
2016-12-05 12:20:04 -0500

    added a switch to toggle validateFile

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 file=$1
 type=$2
+validate=$3  ## 0 : don't validate, just do md5. 1 : validatefastq
 
 if [[ $file =~ \.gz$ ]]
 then
@@ -11,8 +12,12 @@ else
 fi
 
 MD=${MD_OUT[0]}
-validateFiles type=$type $file
-ERCD=$?
 echo "md5sum	$MD" >> report
-echo "validateFiles_errorcode	$ERCD" >> report
+
+if (( $validate == 1 ))
+then
+validateFiles type=$type $file
+  ERCD=$?
+  echo "validateFiles_errorcode	$ERCD" >> report
+fi
 
